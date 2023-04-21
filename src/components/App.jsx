85062c494963888bfa5e6f4d186662af7de9ea8d
Maryna-Harasko <mngarasko@gmail.com>
2023-04-21 22:12:36 +0300

    new contacts and number

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, {Component} from "react";
+import { FormContact } from "./FormContact/FormContact";
+
+export class App extends Component {
+  state = {
+    contacts: [],
+  }
+
+  listenerForm = (data) => {
+    const newContact = { name: data.name, number: data.number };
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  render (){
+    return (
+      <div>
+        <FormContact onSubmit={this.listenerForm}/>
+        <div>
+          <h2>Contacts</h2>
+          <ul>
+          {this.state.contacts.map((contact, index) => (
+            <li key={index}>{contact.name}: {contact.number}</li>
+          ))}
+        </ul>
+        </div>
+      </div>
+    );
+  };
 };
